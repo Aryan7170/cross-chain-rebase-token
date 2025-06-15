acfb72a1a4cc8f9e742901d138a700b0bfe8818e
Aryan Satish <aryanduhoon7170@gmail.com>
2025-06-15 23:23:42 +0530

    fix

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -1,4 +1,4 @@
-//SPDX-LICENSE-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -15,6 +15,7 @@ contract RebaseToken is ERC20 {
     error RebaseToken__IntrestRateCanOnlyDecrease(uint256 oldIntrestRate, uint256 newIntrestRate);
 
     uint256 private s_intrestRate = 5e10;
+    uint256 private constant PRECISION_FATCOR = 1e18;
     mapping(address => uint256) private s_userInterestRate;
     mapping(address => uint256) private s_userLastUpdatedTimestamp;
 
@@ -31,19 +32,29 @@ contract RebaseToken is ERC20 {
 
     }
 
-    function mint(address _to, uint256 _amount) external {
-        _mintAccuredInterest(_to);
-        s_userInterestRate[_to] = s_intrestRate;
-        _mint(_to, _amount);
+    function mint(address _from, uint256 _amount) external {
+        _mintAccuredInterest(_from);
+        s_userInterestRate[_from] = s_intrestRate;
+        _mint(_from, _amount);
 
 
     }
 
+    function burn(address _from, uint256 _amount) external {
+        _mintAccuredInterest(_from);
+        s_userInterestRate[_from] = s_intrestRate;
+        _burn(_from, _amount);
+    }
+
     function _mintAccuredIntrest(address _to) internal {
+        uint256 previousPrincipalBalance = super.balanceOf(_to);
+        uint256 currentBalance = balanceOf(_user);
+        uint256 balanceIncrease = currentBalance - previousPrincipalBalance;
         //Balance of the minted tokens: principal
         //Balance of the minted tokens plus intrest
         //no of tokens to be mintted for paying their intrest
         s_userLastUpdatedTimestamp[_to] = block.timestamp;
+        _mint(_user, balanceIncrease);
     }
 
 
@@ -51,8 +62,18 @@ contract RebaseToken is ERC20 {
         return s_intrestRate;
     }
 
-    function balanceOf(address account) public view override returns (uint256){
-        return super.balanceOf(account) * _calculateUserAccumulatedIntrestSinceLastUpdate(_user);
+    function balanceOf(address account) public view override returns (uint256) {
+        uint256 currentPrincipalBalance = super.balanceOf(account);
+        if (currentPrincipalBalance == 0) {
+            return 0;
+        }
+        return (currentPrincipalBalance * _calculateUserAccumulatedIntrestSinceLastUpdate(_user)) / PRECISION_FACTOR;
+    }
+
+    function _calculateUserAccumulatedIntrestSinceLastUpdate(address _user) internal view returns (linearIntrest) {
+        //Intrest grows linearly over time
+        uint256 timeElaped = block.timestamp - s_userLastUpdatedTimestamp[_user];
+        linearIntrest = PRECISION_FACTOR + (s_userInterestRate[_user] * timeElapsed);
     }
 
 }
