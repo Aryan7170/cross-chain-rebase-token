b209d9944e1d035adb72d9a630d86ed1b911f313
Aryan Satish <aryanduhoon7170@gmail.com>
2025-06-20 22:46:44 +0530

    ADD: interfaces

---
+++ src/Vault.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { IRebaseToken } from "./interfaces/IRebaseToken.sol";
+
+contract Vault {
+
+
+    IRebaseToken private immutable i_rebaseToken;
+
+    error Vault__RedeemFailed();
+
+    event Deposit(address indexed user, uint256 amount);
+    event Redeem(address indexed user, uint256 amount);
+
+    constructor(IRebaseToken _rebaseToken) {
+        i_rebaseToken = _rebaseToken;
+    }
+
+    receive() external payable {}
+
+    function deposit() external payable {
+        i_rebaseToken.mint(msg.sender,msg.value, i_rebaseToken.getUserInterestRate(msg.sender));
+        emit Deposit(msg.sender, msg.value);        
+    }
+
+    function redeem (uint256 _amount) external {
+        i_rebaseToken.burn(msg.sender, _amount);
+        (bool success, ) = payable(msg.sender).call{value: _amount}("");
+        if (!success) {
+            revert Vault__RedeemFailed();
+        }
+        emit Redeem(msg.sender, _amount);
+    }
+
+    function getRebaseToken() external view returns (address) {
+        return address (i_rebaseToken);
+    }
+} 
